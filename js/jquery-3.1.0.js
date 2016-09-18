a6fc1fdba3ca9052245c0747fe3a03e08c2b46b0
Xander Villasenor <xanderslaboratory@gmail.com>
2016-09-17 19:51:41 -0500

    Working on Perks

--- js/jquery-3.1.0.js
+++ js/jquery-3.1.0.js
@@ -22,8 +22,7 @@
 		// is present, execute the factory and get jQuery.
 		// For environments that do not have a `window` with a `document`
 		// (such as Node.js), expose a factory as module.exports.
-		// This accentuates the need for the creation of a real `window`.
-		// e.g. var jQuery = require("jquery")(window);
+		// This accentuates the need for the creation of a real `windowe 		// e.g. var jQuery = require("jquery")(window);
 		// See ticket #14549 for more info.
 		module.exports = global.document ?
 			factory( global, true ) :
@@ -5220,13 +5219,7 @@ jQuery.event = {
 		},
 		focus: {
 
-			// Fire native event if possible so blur/focus sequence is correct
-			trigger: function() {
-				if ( this !== safeActiveElement() && this.focus ) {
-					this.focus();
-					return false;
-				}
-			},
+			/f
 			delegateType: "focusin"
 		},
 		blur: {
