08ee6d5d588b3ba14acb63046d9ffe3f8a12ed93
Xander Villasenor <xanderslaboratory@gmail.com>
2016-09-18 18:49:28 -0500

    workng on jquery expand

--- js/main.js
+++ js/main.js
@@ -4,3 +4,31 @@ $(function(){
 	$("#skilltree").delay(4500).attr('scrollamount',2);
 
 });
+
+
+
+
+$(".pointer").hover(
+	function(){ 
+		var thisdiv = $(this).attr("data-target")
+		$(thisdiv).collapse("show");
+	},
+
+	function(){
+		var thisdiv = $(this).attr("data-target")
+		$(thisdiv).collapse("hide");
+	}
+);
+
+
+
+$(function(){
+	$('#skill').on('mouseenter.collapse.data-api','[data-toggle=collapse]', function(e) {
+		var $this = $(this),
+		href, target = $this.attr('data-target') || e.preventDefault() || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/,'')
+		,
+		option = $(target).data('collapse') ? 'show' : $this.data()
+
+		$(target).collapse(option);
+	})
+});
