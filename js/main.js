03ceb1e4aab3c8ec2f0e9887c48fe86bafe3bdf3
Xander Villasenor <xanderslaboratory@gmail.com>
2016-10-08 23:27:51 -0500

    Major Update
    Most of the layout and script is done, just need to work on actual text

--- js/main.js
+++ js/main.js
@@ -1,19 +1,64 @@
-		function sayHello(who) {
-		document.write("Hello, " + who);
-		}
-
-
-		function Alpha() {
-			
-						var num1 = 534784;
-			var num2 = 3;
-			var total = num1 + num2;
-			document.write(total);
-
-		}
-
 		function skill1(){
-
-			document.getElementById('perkinfo').innerHTML='Test Info';
-
+			document.getElementById('perkinfo').innerHTML="There is more to just speaking Spanish in order for it to be truly beneficial for work. In order to market or provide services to the Hispanic community, their needs to be an understanding of the many distinct Latin cultures. Anyone can use Google translate, but it takes familiarity with the culture to know when to  substitute tamales for pancakes instead of pan dulce.";
 		}
+
+		function skill2(){
+			document.getElementById('perkinfo').innerHTML="The need to wear many hats for nonprofits is an understatement. It wasn't just optimizing or repairing computers, it involved everything from rewiring the building for networks to replacing cracked screens for laptops. Everything from designing databases for students to maintain the organization's website, if it had anything remotely related to technology it fell under my umbrella of responsibility.";
+		}
+
+		function skill3(){
+			document.getElementById('perkinfo').innerHTML="For the last 6 years I have been refurbishing donated computers from companies to provide them for various nonprofit or government programs. Additionally I provided computer repair work to the public, although we never advertised this service, I always had a steady stream of clients through sheer word of mouth. From software to hardware, Apple to Linux, there is very little I can’t fix.";
+		}
+
+		function skill4(){
+			document.getElementById('perkinfo').innerHTML="I’ve not only had to set up our company's network system but my employer would often contract me out to other organizations to set up their network systems. I’ve had a chance to work with a variety of manufactures network devices from school firewall services to speciality routers for public or commercial use. I am familiar with the standard Windows Server 2012 OS and the Linux Ubuntu OS for custom network server setups";
+		}
+
+		function skill5(){
+			document.getElementById('perkinfo').innerHTML="Aside from being proficient in the Microsoft Office Suite, I know how to maximize the flexibility that G-Suite (formerly Google Apps) can offer in the integration of projects, and web content. My strongest skill is interweaving spreadsheet files with formulas that automatically feed into forms, reports, and calculations for grades, budgets or statistics.";
+		}
+		
+		function skill6(){
+			document.getElementById('perkinfo').innerHTML="Most of my website design has been using WordPress with minor edits in the html and css code. I have been studying how to write html5 and css3 with some javascript in order to apply for the front-end position. I hope this webpage is a fair example of what I can do. I can make my own gifs and font type if necessary, I am familiar with vector art and can provide very unique feel for any client.";
+		}
+		
+		function skill7(){
+			document.getElementById('perkinfo').innerHTML="E-Commerce";
+		}
+		
+		function skill8(){
+			document.getElementById('perkinfo').innerHTML="Videography";
+		}
+		
+		function skill9(){
+			document.getElementById('perkinfo').innerHTML="Graphic Artist";
+		}
+		
+		function skill10(){
+			document.getElementById('perkinfo').innerHTML="Software Developer";
+		}
+
+		function skill11(){
+			document.getElementById('perkinfo').innerHTML="ProtoTyping";
+		}
+		
+		function skill12(){
+			document.getElementById('perkinfo').innerHTML="3D Modeling";
+		}
+
+		function skill13(){
+			document.getElementById('perkinfo').innerHTML="Game Designer";
+		}
+
+		function skill14(){
+			document.getElementById('perkinfo').innerHTML="Educator";
+		}
+
+		
+		
+
+		
+
+
+
+
